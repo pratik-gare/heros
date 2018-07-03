2b1ee59c040c37836a7efb368ccd80e6d6299440
pratik_gare <pratik.gare@skyworksinc.com>
2018-07-03 15:09:04 -0700

    Creates new component 'Marvel-heros'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MarvelHerosComponent } from './marvel-heros/marvel-heros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MarvelHerosComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/marvel-heros/marvel-heros.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-marvel-heros',
+  templateUrl: './marvel-heros.component.html',
+  styleUrls: ['./marvel-heros.component.css']
+})
+export class MarvelHerosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
